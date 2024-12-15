4da9fa2aec6dfdd14bd3a95b05455666b2a8ad00
zzZINUZzz <68876749+zzinuzz@users.noreply.github.com>
2024-12-16 00:54:01 +0100

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,7 @@
+<?php  
+define('DB_NAME', 'wordpress');  
+define('DB_USER', 'wordpress');  
+define('DB_PASSWORD', 'wordpress');  
+define('DB_HOST', 'localhost');  
+define('DB_CHARSET', 'utf8mb4');  
+define('DB_COLLATE', '');
