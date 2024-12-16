1b3d450e70420224fa9fc1223d2e6d0abfc8fe56
zzZINUZzz <68876749+zzinuzz@users.noreply.github.com>
2024-12-16 01:04:20 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,7 +1,7 @@
 <?php  
-define('DB_NAME', 'wordpress');  
-define('DB_USER', 'wordpress');  
-define('DB_PASSWORD', 'wordpress');  
-define('DB_HOST', 'localhost');  
+define('DB_NAME', '4260744_zzinuzz');  
+define('DB_USER', '4260744_zzinuzz');  
+define('DB_PASSWORD', '5630201_Pw');  
+define('DB_HOST', 'fdb1027.runhosting.com');  
 define('DB_CHARSET', 'utf8mb4');  
 define('DB_COLLATE', '');
